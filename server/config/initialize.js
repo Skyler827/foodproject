a2a0175da895fac1e64823c8198d58f5558c82a7
Skyler Daché <skyberskyman@gmail.com>
2018-12-17 17:57:38 -0500

    changes to function-- work in progress

--- server/config/initialize.js
+++ server/config/initialize.js
@@ -22,8 +22,18 @@ async function handle_categories() {
 
 async function handle_items() {
     return new Promise((resolve, reject) => {
-
+        Item.deleteMany({}, (err) => {
+            if (err) reject(err);
+            else resolve();
+        });
+    }).then(() => {
+        const json_files = fs.readdirSync("./data/menu_items");
+        return Promise.all(json_files.map(handle_json_file));
     });
+}
+async function handle_json_file(json_filename) {
+    const data = fs.readFileSync(json_filename);
+    return Promise.all(data.map(handle_one_item));
 }
 async function handle_one_item(item_obj) {
     return new Promise((resolve, reject) => {
