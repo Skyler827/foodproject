329ad0fd28c599ffea16bafeeb772b87b54d2a0d
Skyler Daché <skyberskyman@gmail.com>
2018-12-31 16:38:28 -0500

    made some changes

--- server/config/initialize.js
+++ server/config/initialize.js
@@ -1,8 +1,11 @@
 const fs = require("fs");
+const path = require("path");
 const mongoose = require("mongoose");
 const Category = mongoose.model("category");
 const Ingredient = mongoose.model("ingredient");
 const Item = mongoose.model("item");
+
+const menu_items_dir = path.resolve("data","menu_items");
 
 /**
  * This file populates the restaraunt database with food/drink/ingredient/menu data in the supplied JSON files
@@ -11,7 +14,7 @@ async function handle_categories() {
     return new Promise((resolve, reject) => {
         Category.deleteMany({}, (err) => {
             if (err) reject(err);
-            const categories = fs.readdirSync("./data/menu_items").map((s) => s.slice(0,-5)).map((s)=> ({"name":s}));
+            const categories = fs.readdirSync(menu_items_dir).map((s) => s.slice(0,-5)).map((s)=> ({"name":s}));
             Category.create(categories, (err, created_categories) => {
                 if (err) reject(err);
                 else resolve(created_categories);
@@ -32,12 +35,20 @@ async function handle_items() {
     });
 }
 async function handle_json_file(json_filename) {
-    const data = fs.readFileSync(json_filename);
-    return Promise.all(data.map(handle_one_item));
+    return new Promise((resolve, reject)=>
+        fs.readFile(path.join(menu_items_dir,json_filename),{encoding:'utf-8'},(err, data)=>{
+            if (err) reject(err);
+            else resolve(Promise.all(JSON.parse(data).map(handle_one_item)));
+        })
+    );
 }
 async function handle_one_item(item_obj) {
     return new Promise((resolve, reject) => {
-        console.log(item_obj)
+        if (item_obj.hasOwnProperty('ingredients')) resolve();
+        else {
+            console.log("object \""+item_obj.name+"\" missing ingredients!");
+            reject();
+        }
     });
 }
 
