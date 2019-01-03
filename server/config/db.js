34dd539c83b9e5083ae4449ecae4ddc12a369faf
Skyler Daché <skyberskyman@gmail.com>
2019-01-03 01:04:16 -0500

    made db.js require statements windows friendly

--- server/config/db.js
+++ server/config/db.js
@@ -9,7 +9,7 @@ mongoose.connect('mongodb://localhost/foodproject', {
 module.exports.register = function() {
     fs.readdirSync(models).forEach(function(file) {
         if(file.indexOf('.js') >= 0) {
-            require(models + '/' + file);
+            require(path.join(models, file));
         }
     });
 }
