e4fb2ce45701c01072ff001a135671fac1e5dc23
Skyler Daché <skyberskyman@gmail.com>
2019-04-25 19:17:27 -0400

    updated config for easier debugging

--- server/src/config/loggerConfig.ts
+++ server/src/config/loggerConfig.ts
@@ -2,7 +2,7 @@ import {Category,CategoryLogger,CategoryServiceFactory,CategoryConfiguration,Log
  
 // Optionally change default settings, in this example set default logging to Info.
 // Without changing configuration, categories will log to Error.
-CategoryServiceFactory.setDefaultConfiguration(new CategoryConfiguration(LogLevel.Info));
+CategoryServiceFactory.setDefaultConfiguration(new CategoryConfiguration(LogLevel.Warn));
  
 // Create categories, they will autoregister themselves, one category without parent (root) and a child category.
 export const initializeLogger = new Category("initialization");
