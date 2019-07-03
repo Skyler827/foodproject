fc25084f15f17ebbb06ed6c17957f1c82e84aee4
Skyler Daché <skyberskyman@gmail.com>
2019-07-03 19:07:54 -0400

    Worked on layout code

--- client/src/components/dining-room/dining-room.component.ts
+++ client/src/components/dining-room/dining-room.component.ts
@@ -14,8 +14,8 @@ export class DiningRoomComponent implements OnInit {
     diningRoom: DiningRoomWithOrders;
     tableDims: Array<{x:number,y:number,width:number,height:number,number:number,showDialog:boolean,
     color:string,backgroundColor:string}> = [];
-    hmargin: number = 50;
-    vmargin: number = 50;
+    hmargin: number = 10;
+    vmargin: number = 10;
     drHeight: number = 100;
     drWidth: number = 100;
     drTop: number = 100;
@@ -29,7 +29,7 @@ export class DiningRoomComponent implements OnInit {
             this.size();
         });
     }
-    diningRoomHorizontal(): boolean {
+    diningRoomIsHorizontal(): boolean {
         const dr = this.diningRoom;
         const doc = document.documentElement;
         return (dr.width * (doc.clientHeight-verticalMarginPixels-2*this.vmargin) > dr.length * (doc.clientWidth-2*this.hmargin));
@@ -37,20 +37,20 @@ export class DiningRoomComponent implements OnInit {
     @HostListener('window:resize', ['$event'])
     size() {
         const doc = document.documentElement;
-        const h = this.diningRoomHorizontal();
-        const scale = h ?
+        const is_H = this.diningRoomIsHorizontal();
+        const scale = is_H ?
             doc.clientWidth/this.diningRoom.width:
             (doc.clientHeight - verticalMarginPixels)/this.diningRoom.length;
-        const x0 = h ? this.hmargin : (doc.clientWidth - 2*this.hmargin - scale * this.diningRoom.width)/2;
-        const y0 = h ? (doc.clientHeight - verticalMarginPixels - 2*this.vmargin - scale * this.diningRoom.length)/2 : this.vmargin;
-        this.drHeight = h ?
+        const x0 = is_H ? this.hmargin : (doc.clientWidth - 2*this.hmargin - scale * this.diningRoom.width)/2;
+        const y0 = is_H ? (doc.clientHeight - verticalMarginPixels - 2*this.vmargin - scale * this.diningRoom.length)/2 : this.vmargin;
+        this.drHeight = is_H ?
             scale * this.diningRoom.length :
             doc.clientHeight - verticalMarginPixels;
-        this.drWidth  = h ?
+        this.drWidth = is_H ?
             doc.clientWidth :
             scale * this.diningRoom.width;
         this.tableDims = this.diningRoom.tables.map(t => ({
-            x: scale* t.x ,
+            x: scale * t.x,
             y: scale * t.y,
             width: scale * t.width,
             height: scale * t.height,
